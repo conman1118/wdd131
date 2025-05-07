79463ca9222b09e357af709faefe10b900c13dd8
conman1118 <connor.f.neus@gmail.com>
2025-05-07 15:02:17 -0700

    commit all

---
+++ scripts/getdates.js
@@ -0,0 +1,20 @@
+// sets variable year - finds HTML element with the id of currentyear and stores the reference to that element
+const year = document.querySelector("#currentyear");
+
+// sets variable today - creates a data object and stores it
+const today = new Date();
+
+/* takes the year variable and inserts it into the HTML - today.getFullYear extracts just the year and enters it inside the <span> tag.
+Inner HTML updates that selected element with the formatted content. */
+year.innerHTML = `<span class="currentyear">${today.getFullYear()}</span>`;
+
+
+
+
+// JAvaScript finds the element with the id of lastModified then sets the content of that element to the document.lastModiefied date/time
+
+// set variable for the lsat modified date time
+let lastModDate = new Date(document.lastModified);
+
+
+document.getElementById("lastModified").textContent = "Last Modified: " + lastModDate.toLocaleString();
